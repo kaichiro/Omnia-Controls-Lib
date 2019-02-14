14c93d2dc320d0afa3c312cb9e5355195b110f01
kaichiro <kcfg23@gmail.com>
2019-02-14 11:39:05 -0200

    Atualizando automatismos

--- pull.bat
+++ pull.bat
@@ -1,11 +1,16 @@
 git checkout master
 git pull azure master
+git pull origin master
 git checkout ocl
 git pull azure ocl
+git pull origin ocl
 git checkout sql
 git pull azure sql
+git pull origin sql
 git checkout report
 git pull azure report
+git pull origin report
 git checkout XML_imports
 git pull azure XML_imports
+git pull origin XML_imports
 git checkout master
--- push.bat
+++ push.bat
@@ -1,11 +1,16 @@
 git checkout master
 git push origin master
+git push azure master
 git checkout ocl
 git push origin ocl
+git push azure ocl
 git checkout sql
 git push origin sql
+git push azure sql
 git checkout report
 git push origin report
+git push azure report
 git checkout XML_imports
 git push origin XML_imports
+git push azure XML_imports
 git checkout master
