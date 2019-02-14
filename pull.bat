484d4c9bd1623df48e32fe93e0dea47b6cca01b0
kaichiro <kcfg23@gmail.com>
2019-02-14 11:10:09 -0200

    Adicionando automatismo para PULL e PUSH

---
+++ pull.bat
@@ -0,0 +1,11 @@
+git checkout master
+git pull azure master
+git checkout ocl
+git pull azure ocl
+git checkout sql
+git pull azure sql
+git checkout report
+git pull azure report
+git checkout XML_imports
+git pull azure XML_imports
+git checkout master
---
+++ push.bat
@@ -0,0 +1,11 @@
+git checkout master
+git pull origin master
+git checkout ocl
+git pull origin ocl
+git checkout sql
+git pull origin sql
+git checkout report
+git pull origin report
+git checkout XML_imports
+git pull origin XML_imports
+git checkout master
