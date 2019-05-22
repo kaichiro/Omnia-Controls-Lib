34a5320887535dc67f38b7552d40f4d2a0561cf3
kaichiro <kcfg23@gmail.com>
2019-05-22 15:36:56 -0300

    Refatorando comandos SQL para unificar registros de médicos

--- Commands/iLabor.NET/SQL/02-usolando.registro.de.medico-02.sql
+++ Commands/iLabor.NET/SQL/02-usolando.registro.de.medico-02.sql
@@ -1,5 +1,11 @@
 /* Individualizando/isolando estidade Médico */
 
+declare @pCRM varchar(16) = '52158907';
+declare @pNome varchar(128) = 'AODO LAINETT';
+declare @pSigla_Conselho varchar(16) = 'CRM';
+declare @pUF_Conselho varchar(8) = '';
+
+
 use iLabor;
 
 select 
@@ -12,8 +18,8 @@ md.ECO_ID
 , (select COUNT(p.ECO_ID) from Protocolo p where md.ECO_ID = p.Medico) QuantidadeProtocolos
 , (select COUNT(pp.ECO_ID) from Protocolo_Procedimento pp inner join Protocolo_ProcedimentoProtocolo_ProcedimentosMedicoMedicos ppm on pp.ECO_ID = ppm.Protocolo_Procedimentos where md.ECO_ID = ppm.Medicos) QuantidadeProtocolos
 from Medico md
-where md.CRM = ''
-and md.Nome = 'HELIAN '
-and md.Sigla_Conselho = 'CRM'
-and md.UF_Conselho = ''
+where md.CRM = @pCRM
+and md.Nome = @pNome
+and md.Sigla_Conselho = @pSigla_Conselho
+and md.UF_Conselho = @pUF_Conselho
 order by 7 desc, 8 desc;
